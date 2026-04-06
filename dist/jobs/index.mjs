d565d08b1e59c5a5abda36dcaf85982dcf998a83
Ramesh Tamilselvan <rameshtamilselvan@gmail.com>
2026-04-07 00:33:23 +0400

    Clean generated SDK artifacts and harden regex paths

--- dist/jobs/index.mjs
+++
@@ -1,7 +0,0 @@
-import {
-  JobsModule
-} from "../chunk-V22XEMYR.mjs";
-import "../chunk-74DCVGBD.mjs";
-export {
-  JobsModule
-};
--- dist/seals/index.mjs
+++
@@ -1,6 +0,0 @@
-import {
-  SealsModule
-} from "../chunk-DCONG3Q6.mjs";
-export {
-  SealsModule
-};
